d0469e84cee8cbf9278aac30f7089ecc43b89f45
Amnon Amir <amnonim@gmail.com>
2016-03-16 12:32:05 -0700

    flake8 fix

--- deblur/test/test_workflow.py
+++ deblur/test/test_workflow.py
@@ -51,14 +51,13 @@ class workflowTests(TestCase):
         self.working_dir = mkdtemp()
 
         # the data directory for the workflow test files
-        self.test_data_dir = join(dirname(abspath(__file__)),'data')
-        self.seqs_s1_fp = join(self.test_data_dir,'seqs_s1.fa')
-        self.seqs_s2_fp = join(self.test_data_dir,'seqs_s2.fa')
-        self.orig_s1_fp = join(self.test_data_dir,'simset.s1.fasta')
-        self.orig_s2_fp = join(self.test_data_dir,'simset.s2.fasta')
+        self.test_data_dir = join(dirname(abspath(__file__)), 'data')
+        self.seqs_s1_fp = join(self.test_data_dir, 'seqs_s1.fa')
+        self.seqs_s2_fp = join(self.test_data_dir, 'seqs_s2.fa')
+        self.orig_s1_fp = join(self.test_data_dir, 'simset.s1.fasta')
+        self.orig_s2_fp = join(self.test_data_dir, 'simset.s2.fasta')
 
-        self.files_to_remove=[]
-
+        self.files_to_remove = []
 
     def tearDown(self):
         remove_files(self.files_to_remove)
@@ -569,7 +568,6 @@ H\t2\t100\t100.0\t*\t0\t0\t*\ts1_13\ts1_10
         files_to_remove_act = [basename(f) for f in files_to_remove]
         self.assertListEqual(files_to_remove_exp, files_to_remove_act)
 
-
     def test_launch_workflow(self):
         """Test launching complete workflow using 2 simulated sequence files.
         seqs1 - 100 reads using art, original sequences are >0.5 identical.
@@ -577,16 +575,17 @@ H\t2\t100\t100.0\t*\t0\t0\t*\ts1_13\ts1_10
         0.1 chimeras, 35 phix reads
         """
         # index the 70% rep. set database
-        ref_fp = join(self.test_data_dir,'70_otus.fasta')
+        ref_fp = join(self.test_data_dir, '70_otus.fasta')
         ref_db_fp, files_to_remove = build_index_sortmerna(
             ref_fp=(ref_fp,),
             working_dir=self.working_dir)
 
-        self.run_workflow_test(self.seqs_s1_fp,self.orig_s1_fp,ref_fp,ref_db_fp)
-        self.run_workflow_test(self.seqs_s2_fp,self.orig_s2_fp,ref_fp,ref_db_fp)
+        self.run_workflow_test(self.seqs_s1_fp,
+                               self.orig_s1_fp, ref_fp, ref_db_fp)
+        self.run_workflow_test(self.seqs_s2_fp,
+                               self.orig_s2_fp, ref_fp, ref_db_fp)
 
-
-    def run_workflow_test(self,simfilename,origfilename,ref_fp,ref_db_fp):
+    def run_workflow_test(self, simfilename, origfilename, ref_fp, ref_db_fp):
         """Test launching the complete workflow using simulated sequences
         and compare to original ground truth.
 
@@ -609,14 +608,14 @@ H\t2\t100\t100.0\t*\t0\t0\t*\ts1_13\ts1_10
         threads = 1
         delim = '_'
         biom_fp = launch_workflow(seqs_fp, output_fp, read_error, mean_error,
-                                  error_dist, indel_prob, indel_max, trim_length,
-                                  min_size, (ref_fp,), ref_db_fp, negate,
-                                  threads, delim)
+                                  error_dist, indel_prob, indel_max,
+                                  trim_length, min_size, (ref_fp,),
+                                  ref_db_fp, negate, threads, delim)
 
         # get the trimmed ground truth sequences
         with open(origfilename, 'U') as f:
             orig_seqs = [item[1] for item in parse_fasta(f)]
-        orig_seqs=[item[:trim_length].upper() for item in orig_seqs]
+        orig_seqs = [item[:trim_length].upper() for item in orig_seqs]
 
         table_obs = load_table(biom_fp)
         outseqs = table_obs.ids(axis='observation')
@@ -628,7 +627,6 @@ H\t2\t100\t100.0\t*\t0\t0\t*\ts1_13\ts1_10
         self.assertTrue(foundall)
         # and don't see any additional sequences
         self.assertTrue(len(outseqs) == len(orig_seqs))
-
 
     def get_seqs_act_split_sequence_on_sample_ids(self, output_dir):
         """Parse output of split_sequence_file_on_sample_ids_to_files()
