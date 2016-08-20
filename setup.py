7b656a40278eadc83b33d6f93a378eff9a0585dd
Daniel McDonald <mcdonadt@colorado.edu>
2016-08-19 17:09:13 -0700

    DOC: classification mention

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ classes = """
     Topic :: Software Development :: Libraries :: Application Frameworks
     Topic :: Software Development :: Libraries :: Python Modules
     Programming Language :: Python
-    Programming Language :: Python :: 2.7
+    Programming Language :: Python :: 3.5
     Programming Language :: Python :: Implementation :: CPython
     Operating System :: POSIX :: Linux
     Operating System :: MacOS :: MacOS X
