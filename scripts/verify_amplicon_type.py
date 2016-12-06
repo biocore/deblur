58ec12fdfc7893e1fbe8f08c6fe2dabd3bff3bf4
Luke Thompson <lukethompson@gmail.com>
2016-12-06 13:09:48 -0800

    added support for 18S and ITS, now explicitly calling k-mers tetramers

--- scripts/verify_amplicon_type.py
+++ scripts/verify_amplicon_type.py
@@ -13,7 +13,7 @@ import numpy as np
 import pandas as pd
 
 def count_starting_kmers(input_fasta_fp, num_seqs, seed):
-    """Generate value_counts dataframe of starting k-mers for random subsample
+    """Generate value_counts dataframe of 5' tetramers for random subsample
     of a fasta file"""
     kmer_length = 4
     if seed:
@@ -43,40 +43,43 @@ def count_starting_kmers(input_fasta_fp, num_seqs, seed):
               help="Number of sequences to randomly subsample [default: 10000]")
 @click.option('--cutoff', '-c', required=False, type=float, default=0.5,
               help="Minimum fraction of sequences required to match "
-                   "a diagnostic k-mer [default: 0.5]")
+                   "a diagnostic 5' tetramer [default: 0.5]")
 @click.option('--seed', '-s', required=False, type=int, 
               help="Random number seed [default: None]")
 
 def verify_amplicon_type(input_fasta_fp, num_seqs, cutoff, seed):
     """Determine the most likely amplicon type of a fasta file based on the
-    first few nucleotides.
+    first four nucleotides.
 
-    The most frequent starting k-mer in a random subsample of sequences must 
+    The most frequent 5' tetramer in a random subsample of sequences must 
     match, above a given cutoff fraction of sequences, one of the following  
-    diagnostic k-mers:
+    diagnostic tetramers:
 
-      k-mer\tAmplicon\tForward primer                             
+      Tetramer\tAmplicon\tForward primer                             
       TACG\t16S rRNA\t515f                                       
-      NNNN\t18S rRNA\tEuk1391f                                    
-      NNNN\tITS rRNA\tITS1f                                       
+      GTAG\tITS rRNA\tITS1f                                       
+      GCT[AC]\t18S rRNA\tEuk1391f                                    
     """
     starting_kmer_value_counts = count_starting_kmers(input_fasta_fp, num_seqs, 
       seed)
     top_kmer = starting_kmer_value_counts.index[0]
     top_kmer_count = starting_kmer_value_counts[0]
     top_kmer_frac = top_kmer_count/num_seqs
+    second_kmer = starting_kmer_value_counts.index[1]
+    second_kmer_count = starting_kmer_value_counts[1]
+    top2_kmer_frac = (top_kmer_count+second_kmer_count)/num_seqs
     if (top_kmer == 'TACG') & (top_kmer_frac > cutoff):
         print('Amplicon type: 16S/515f (%s%% of sequences start with %s)' %
               (round(top_kmer_frac*100, 1), top_kmer))
-    elif (top_kmer == 'NNNN') & (top_kmer_frac > cutoff):
-        print('Amplicon type: 18S/Euk1391f (%s%% of sequences start with %s)' %
-              (round(top_kmer_frac*100, 1), top_kmer))
-    elif (top_kmer == 'NNNN') & (top_kmer_frac > cutoff):
+    elif (top_kmer == 'GTAG') & (top_kmer_frac > cutoff):
         print('Amplicon type: ITS/ITS1f (%s%% of sequences start with %s)' %
               (round(top_kmer_frac*100, 1), top_kmer))
+    elif (top_kmer in ['GCTA', 'GCTC']) & (second_kmer in ['GCTA', 'GCTC']) & (top2_kmer_frac > cutoff):
+        print('Amplicon type: 18S/Euk1391f (%s%% of sequences start with %s or %s)' %
+              (round(top2_kmer_frac*100, 1), top_kmer, second_kmer))
     else:
         print('Could not determine amplicon type'),
-        print('(most frequent starting k-mer was %s with %s%%)' %
+        print('(most frequent starting tetramer was %s with %s%%)' %
               (top_kmer, round(top_kmer_frac*100, 1)))
 
 if __name__ == '__main__':
